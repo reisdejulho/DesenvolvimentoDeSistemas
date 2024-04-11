477877c4b31643263ad6e3eda5e78d0e11d54304
João Gabriel <144185416+reisdejulho@users.noreply.github.com>
2024-04-11 11:01:38 -0300

    Update template.php

--- template.php
+++ template.php
@@ -1,30 +1,18 @@
 <html>
-<head>
-<meta charset="utf-8" />
-<title>Gerenciador de Tarefas</title>
-<link rel="stylesheet" href="tarefas.css" type="text/css" />
-</head>
-<body>
-<h1>Gerenciador de Tarefas</h1>
-<form>
-<fieldset>
-<legend>Nova tarefa</legend>
-<label>
-Tarefa:
-<input type="text" name="nome" />
-</label>
-<input type="submit" value="Cadastrar" />
-</fieldset>
-</form>
-<table>
-<tr>
-<th>Tarefas</th>
-</tr>
-<?php foreach ($lista_tarefas as $tarefa) : ?>
-<tr>
-<td><?php echo $tarefa; ?> </td>
-</tr>
-<?php endforeach; ?>
-</table>
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <title>Gerenciador de Tarefas</title>
+        <link rel="stylesheet" href="tarefas.css" type="text/css" />
+    </head>
+    <body>
+        <h1>Gerenciador de Tarefas</h1>
+
+
+        <?php include('formulario.php'); ?>
+
+
+        <?php if ($exibir_tabela) : ?>
+            <?php include('tabela.php'); ?>
+        <?php endif; ?>
+    </body>
 </html>
